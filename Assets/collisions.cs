1af8b2aa44386baccb7eb448c7156d9a1d46f40b
minli-45 <amine.labidi004@gmail.com>
2025-02-02 04:44:29 -0500

    added borders and ceiling
    Borders reset position of current piece while ceiling reloads the scene (restart)

--- Assets/collisions.cs
+++ Assets/collisions.cs
@@ -1,17 +1,37 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class collisions : MonoBehaviour
 {
     public bool canFall = false;
+    public bool reset = false;
+    public bool stop = false;
     movement controller;
     void Start() {
         controller = GetComponent<movement>();
     }
     void OnCollisionEnter(Collision other)
     {
-        GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
-        GetComponent<Rigidbody>().useGravity = true;
-        controller.enabled = false;
-        canFall = true;
+        if (other.gameObject.tag == "Box" || other.gameObject.tag == "Ground"){
+            GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            GetComponent<Rigidbody>().useGravity = true;
+            controller.enabled = false;
+            canFall = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Border") {
+            reset = true;
+        }
+
+        if (other.gameObject.tag == "Ceiling" && !controller.enabled) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Ceiling" && !controller.enabled) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -8,9 +8,16 @@ public class spawner : MonoBehaviour
     private Vector3 scaleChange;
     private Vector3 centerOffset;
     private bool BoxFalling = false;
+    private Vector3 restartingPosition;
+    private int nbBoxes;
     Vector3 randomSize() {
-        Vector3 boxSize = new Vector3((1f/Random.Range(2,6))*PalletSideLength, Random.Range(0f, 0.5f),(1f/Random.Range(2,6))*PalletSideLength);
+        Vector3 boxSize = new Vector3((1f/Random.Range(2,6))*PalletSideLength, Random.Range(0.1f, 0.5f),(1f/Random.Range(2,6))*PalletSideLength);
         return boxSize;
+    }
+
+    void resetBox() {
+        box.transform.position = restartingPosition;
+        box.GetComponent<collisions>().reset = false;
     }
 
     void Update()
@@ -21,10 +28,14 @@ public class spawner : MonoBehaviour
             box = Instantiate(BasicBox, transform.position + centerOffset, Quaternion.identity);
             box.transform.localScale += scaleChange;
             box.GetComponent<movement>().enabled = true;
+            restartingPosition = box.transform.position;
             BoxFalling = true;
         }
         if (box.GetComponent<collisions>().canFall) {
             BoxFalling = false;
+        }
+        if (box.GetComponent<collisions>().reset) {
+            resetBox();
         }
     }
 }
